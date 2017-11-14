4fea9a97be910b36b54590f29754c3b46572f1ba
Yildirim-Habr <30645972+Yildirim-Habr@users.noreply.github.com>
2017-11-14 22:31:56 +0700

    Create Who_is_highest_and_how_many.cpp

---
+++ Who_is_highest_and_how_many.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int NE, SE, NW, SW;
+    int i, x, z;
+    double sls, maxv;
+
+    struct store{
+        string nm;
+        double v;
+    };
+
+    store a[4];
+    a[0].nm = "NE";
+    a[1].nm = "SE";
+    a[2].nm = "NW";
+    a[3].nm = "SW";
+
+    maxv = 0.00;
+    for(i = 0; i < 4; i++){
+        cout << "Enter the total sales for " << a[i].nm << " is = $"  ;
+        cin >> sls;
+        a[i].v = sls;
+
+        if(sls < 0.00){
+            cout << "Don't enter the negative number" << endl;
+        }
+
+        if(a[i].v > maxv){
+            maxv = a[i].v;
+            z = i;
+        }
+    }
+
+    cout << "The highest total sales is regarded by " << a[z].nm << " with $" << maxv << endl;
+
+    return 0;
+}
