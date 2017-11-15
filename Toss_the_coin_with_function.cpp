cd69dfed6ac1d1f2dd2dfcdc6526a208950b4f1f
Yildirim-Habr <30645972+Yildirim-Habr@users.noreply.github.com>
2017-11-15 18:56:42 +0700

    Create Toss_the_coin_with_function.cpp

---
+++ Toss_the_coin_with_function.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int coinToss(int in){
+    int r = rand() % 2 + 1;
+    return r;
+}
+
+void start(){
+    int in, i;
+
+    cout << "How many times to toss the coin? ";
+    cin >> in;
+
+    for(i = 0; i < in; i++){
+        if(coinToss(in) == 1){
+            cout << "Head" << endl;
+        }else{
+            cout << "Tail" << endl;
+        }
+    }
+}
+
+int main() {
+    start();
+
+    return 0;
+}
