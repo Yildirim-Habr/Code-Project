afd8f1938a3c124fc48f55e3a4af4847ab51bd42
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2018-04-23 17:10:12 +0700

    Create boutique2.cpp

---
+++ boutique2.cpp
@@ -0,0 +1,90 @@
+#include <iostream>  
+
+using namespace std;  
+
+int main() 
+{
+int a, b, c, i, j, k, l, m, n, core, length, width;
+
+core = 3;
+length = 5;
+width = 12;
+
+cout << endl;
+
+//above wall
+cout << "O";
+for(j = 0; j < 2 + width + ((width+1)*(core*2-1)); j++){
+	cout << "=";
+}
+cout << "O" << endl;
+
+//boutique
+for(n = 0; n < length; n++){
+	//above part
+	for(i = 1; i <= core; i++){
+		cout << "|";
+		//left rest
+		for(j = i; j <= core; j++){
+			cout << "#";
+		}
+		
+		for(j = i; j <= core; j++){
+			cout << "-";
+		}	
+		//mid
+		for(l = 0; l < width; l++){			
+			for(k = 0; k < i*2-1; k++){
+				cout << "#";
+			}
+			
+			for(j = core*2-i; j > i-1; j--){
+				cout << "-";
+			}
+		}
+		//additional part
+		for(k = 0; k < i*2-1; k++){
+			cout << "#";
+		}
+		cout << "|" << endl;
+	}
+	
+	//below part
+	for(i = 1; i <= core; i++){
+		cout << "|";
+		//left rest
+		for(j = 0; j < i; j++){
+			cout << "-";
+		}
+		//mid
+		for(l = 0; l < width; l++){
+			for(j = core*2-i; j > i-1; j--){
+				cout << "#";
+			}
+			
+			for(k = 0; k < i*2-1; k++){
+				cout << "-";
+			}
+		}
+		//additional part
+		for(j = core*2-i; j > i-1; j--){
+			cout << "#";
+		}
+		//right rest
+		for(m = 0; m < i; m++){
+			cout << "-";
+		}
+		
+		cout << "|" << endl;
+	}
+}
+
+//below wall
+cout << "O";
+for(j = 0; j < 2 + width + ((width+1)*(core*2-1)); j++){
+	cout << "=";
+}
+cout << "O";
+
+return 0;
+}
