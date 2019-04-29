040f523bd5fe4594333a098e8d1a36d4bf23f573
yildirim_habr <30645972+Yildirim-Habr@users.noreply.github.com>
2019-04-30 02:06:29 +0700

    Create cpp_template.cpp

---
+++ cpp_template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define eb emplace_back
+#define all(a) (a).begin(), (a).end()
+#define sz(a) ((int)(a).size())
+#define fi first 
+#define se second
+
+using namespace std;
+using i64 = long long;
+using db = double;
+
+const int N9 = 1e9 + 7;
+const int N5 = 1e5 + 5;
+const db PI = acos(-1);
+const bool debug = 1;
+
+template<typename T> void debug_all (vector<T> arr) {for (T x : arr) {cerr << x << ' ';}}
+
+// always checking the querry first.................. (^_^)
+// always checking all edge value possibility (0 - N)
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    cout.precision(12);
+    cout << fixed;
+    
+    int t = 1;
+    //cin >> t;
+
+    while (t--) {
+        solve();
+        cout << '\n';
+    }
+    
+    cerr << endl << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s.\n";
+    return 0;
+}
